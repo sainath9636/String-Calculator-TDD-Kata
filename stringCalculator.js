24b2fd54a718b1d4e4b772d476779b8d57a45162
Sai Kumar Chikkalavari <64541827+sainath9636@users.noreply.github.com>
2025-09-20 13:50:03 +0530

    delimeter handling added

--- stringCalculator.js
+++ stringCalculator.js
@@ -4,8 +4,18 @@ class StringCalculator {
       return 0;
     }
 
-    return numbers
-      .split(/[,\n]/)
+    let delimiter = /[,\n]/; 
+    let numbersToProcess = numbers;
+
+    if (numbers.startsWith('//')) {
+      const delimiterEndIndex = numbers.indexOf('\n');
+      const customDelimiter = numbers.substring(2, delimiterEndIndex);
+      numbersToProcess = numbers.substring(delimiterEndIndex + 1);
+      delimiter = customDelimiter;
+    }
+
+    return numbersToProcess
+      .split(delimiter)
       .map(num => parseInt(num))
       .reduce((sum, num) => sum + num, 0);
   }
